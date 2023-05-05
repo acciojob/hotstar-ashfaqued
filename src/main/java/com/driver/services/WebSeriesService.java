8746033b25edbd76bd74361226f25ef26106f385
Ashfaque <ashfaque.d@gmail.com>
2023-05-05 21:59:56 +0530

    webservice updated

--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -23,8 +23,34 @@ public class WebSeriesService {
         //Incase the seriesName is already present in the Db throw Exception("Series is already present")
         //use function written in Repository Layer for the same
         //Dont forget to save the production and webseries Repo
+WebSeries webSeries= webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName());
+ if(webSeries!=null)
+ {
+     throw new Exception("Series is already present");
+ }
+   WebSeries webSeriesNew = new WebSeries();
+ webSeriesNew.setSeriesName(webSeriesEntryDto.getSeriesName());
+ webSeriesNew.setAgeLimit(webSeriesEntryDto.getAgeLimit());
+ webSeriesNew.setRating(webSeriesEntryDto.getRating());
+ webSeriesNew.setSubscriptionType(webSeriesEntryDto.getSubscriptionType());
 
-        return null;
+ ProductionHouse productionHouse = productionHouseRepository.findById(webSeriesEntryDto.getProductionHouseId()).get();
+
+ webSeriesNew.setProductionHouse(productionHouse);
+
+ productionHouse.getWebSeriesList().add(webSeriesNew);
+
+ double oldRating = productionHouse.getRatings();
+ double newRating=webSeriesNew.getRating();
+ //int listSize = productionHouse.getWebSeriesList().size();
+
+ double avRating= (oldRating+newRating)/2;
+   productionHouse.setRatings(avRating);
+
+   productionHouseRepository.save(productionHouse);
+   //webSeriesNew.setProductionHouse(productionHouse);
+     int n= webSeriesRepository.save(webSeriesNew).getId();
+        return n;
     }
 
 }
